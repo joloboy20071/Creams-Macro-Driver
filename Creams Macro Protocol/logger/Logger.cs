2e88284d61be36b5a96a22f44f716dad7decd10f
joloboy20071 <Milan@creemers-bruggink.com>
2025-05-28 06:31:48 +0200

    Added logger class

---
+++ Creams Macro Protocol/logger/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Creams_Macro_Protocol.logger
+{
+    internal static class Logger
+    {
+        readonly static String logPath = @".\logs\log-Last.txt";
+
+        private static bool AppendToFile(string text) {
+            
+            if (!File.Exists(logPath)) { File.Create(logPath); }
+            
+            using (StreamWriter fileobj = File.AppendText(logPath))
+            {
+                try
+                {
+                    fileobj.Write(text);
+                    return true;
+                }
+                catch { return false; }
+            }
+        }
+
+        public static void Debug(string text) { AppendToFile($"[Debug]: {text}\n"); }
+        
+        public static void Info(string text) { AppendToFile($"[Info]: {text}\n"); }
+
+        public static void Warn(string text) { AppendToFile($"[Warn]: {text}\n"); }
+
+        public static void Error(string text) { AppendToFile($"[Error]: {text}\n"); }
+
+
+
+
+    }
+}
